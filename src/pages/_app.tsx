985d2588c33195775afbaf72ccac835b26d229cd
reuelteodoro <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>
2021-09-06 20:20:48 +1000

    Initial for i18n

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,29 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import NextApp from 'next/app';
+import type {AppContext, AppProps} from 'next/app'
+import {NextIntlProvider} from 'next-intl';
 import {AuthProvider} from "../contexts/AuthContext";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+type MyAppPros = AppProps & { messages: object };
+
+function MyApp({Component, messages, pageProps}: MyAppPros) {
+    return (
+        <NextIntlProvider messages={{...messages, ...pageProps.messages}}>
+            <AuthProvider>
+                <Component {...pageProps} />
+            </AuthProvider>
+        </NextIntlProvider>
     );
 }
+
+MyApp.getInitialProps = async function getInitialProps(context: AppContext) {
+    const {locale} = context.router;
+    console.log(locale);
+
+    return {
+        ...(await NextApp.getInitialProps(context)),
+        messages: require(`../../lang/${locale}.json`)
+    };
+};
+
 export default MyApp
