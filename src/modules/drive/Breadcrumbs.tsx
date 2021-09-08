aaff7a2cdf994f2921cff1f92f5cc7df8a0f8b67
reuelteodoro <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>
2021-09-09 08:24:29 +1000

    Added breadcrumbs and quick fix

--- src/modules/drive/Breadcrumbs.tsx
+++ src/modules/drive/Breadcrumbs.tsx
@@ -1,12 +1,23 @@
-import {FunctionComponent} from "react";
+import React,{FunctionComponent} from "react";
+import Link from "next/link";
+import route from "../../constants/route.json";
 import {folderType, ROOT_FOLDER} from "../../hooks/useFolder";
 
-const Breadcrumbs: FunctionComponent<{currentFolder:folderType}> = ({currentFolder}) => {
-    let path = currentFolder === ROOT_FOLDER ? [] : [ROOT_FOLDER];
-    // if(currentFolder) path = [...path, ...currentFolder.path]
+const Breadcrumbs: FunctionComponent<{ currentFolder: folderType }> = ({currentFolder}) => {
+    let path: Array<null | {name: string, id: null | string}> = currentFolder === ROOT_FOLDER ? [] : [ROOT_FOLDER];
+    if (currentFolder) path = [...path, ...currentFolder.path]
 
     return <div>
-        {path.length > 0 && path.map(path => <span key={path.id}>{path.name}</span>)}
+        {path.map((path, index) =>
+            <React.Fragment key={index}>
+                <Link href={!path?.id ? route.DRIVE : `${route.DRIVE}/${path.id}`} >
+                    <a>
+                        {path?.name}
+                    </a>
+                </Link>
+                &nbsp;
+            </React.Fragment>
+        )}
         {currentFolder && (<span>{currentFolder.name}</span>)}
     </div>
 }
