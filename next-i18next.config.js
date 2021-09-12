5dd94bbe2b57f2670ca0be2fe1e0cf726f22ac3a
reuelteodoro <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>
2021-09-12 14:28:27 +1000

    Better translation library

---
+++ next-i18next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  i18n: {
+    defaultLocale: "en-AU",
+    locales: ["en-AU"]
+  },
+  reloadOnPrerender: process.env.NODE_ENV === "development"
+};
--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config')
+
 module.exports = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en-AU'],
-    defaultLocale: 'en-AU',
-  },
+  i18n
 }
